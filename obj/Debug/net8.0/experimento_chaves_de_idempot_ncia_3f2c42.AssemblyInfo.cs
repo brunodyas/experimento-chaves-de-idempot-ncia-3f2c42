7e108d56848ca7ee830e611c950fbda22760c92b
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 11:40:26 -0300

    feat: entrada CLI mínima
    intent: feature

--- obj/Debug/net8.0/experimento_chaves_de_idempot_ncia_3f2c42.AssemblyInfo.cs
+++ obj/Debug/net8.0/experimento_chaves_de_idempot_ncia_3f2c42.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("experimento_chaves_de_idempot_ncia_3f2c42")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51e4af329126ec16a35417c2568de8163fee0a63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86b3310d2b2d26c533433e7505298b8d3005532d")]
 [assembly: System.Reflection.AssemblyProductAttribute("experimento_chaves_de_idempot_ncia_3f2c42")]
 [assembly: System.Reflection.AssemblyTitleAttribute("experimento_chaves_de_idempot_ncia_3f2c42")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
